110d6385024065a76fe5614063761a8c17b42250
Duvan Orozco <metrologiasam@gmail.com>
2025-09-10 15:42:34 -0500

    prueba3

--- proyecto_c/settings.py
+++ proyecto_c/settings.py
@@ -160,6 +160,9 @@ if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
     AWS_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
+    # ✅ Aquí está la clave
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
     if RENDER_EXTERNAL_HOSTNAME:
         STATICFILES_STORAGE = 'proyecto_c.storages.S3StaticStorage'
         AWS_STATIC_LOCATION = 'static'
