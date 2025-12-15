047184d02df5939b995ee205f8e6edb7e4765589
Duvan Orozco <metrologiasam@gmail.com>
2025-12-15 09:08:21 -0500

    Actualizar nombre del bucket de backups a sam-metrologia-backups1

--- backup_to_s3.py
+++ backup_to_s3.py
@@ -40,7 +40,7 @@ class DatabaseBackupManager:
 
     def __init__(self):
         self.database_url = os.environ.get('DATABASE_URL')
-        self.s3_bucket = os.environ.get('AWS_BACKUP_BUCKET', 'sam-metrologia-backups')
+        self.s3_bucket = os.environ.get('AWS_BACKUP_BUCKET', 'sam-metrologia-backups1')
         self.s3_client = boto3.client('s3',
             aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
